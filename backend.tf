cfd77cfcd902e930b5ec455139f52504b37e3e76
sourav kundu <sourav12kundu@gmail.com>
2021-11-01 06:59:11 -0500

    removed backend state file info

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "skundu-tg-rs-poc"
-    encrypt        = true
-    key            = "tf/project-31/terraform.tfstate"
-    region         = "us-east-2"
-  }
-}
