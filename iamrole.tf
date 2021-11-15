bfd0633c13305972c9e2d252cfff655af6b64316
sourav kundu <sourav12kundu@gmail.com>
2021-11-15 13:29:50 -0600

    tightened policy by removing extra permission

--- iamrole.tf
+++ iamrole.tf
@@ -10,13 +10,6 @@ resource "aws_iam_policy" "ec2_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "ssm:DescribeParameters"
-        ],
-        Resource = "*"
-      },
-      {
         Effect = "Allow",
         Action = [
           "ssm:GetParameters",
