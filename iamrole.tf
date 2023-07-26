ce944979059cf769507d72bcbdf8c19cc0843970
Sourav Kundu <skundu.dev@gmail.com>
2023-07-26 17:13:31 -0500

    corrected the code

--- iamrole.tf
+++ iamrole.tf
@@ -53,14 +53,6 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-# #Attach role to policy
-# #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy_attachment
-# resource "aws_iam_policy_attachment" "ec2_policy_role" {
-#   name       = "ec2_attachment"
-#   roles      = [aws_iam_role.ec2_role.name]
-#   policy_arn = aws_iam_policy.ec2_policy.arn
-# }
-
 #Attach role to policy
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "custom" {
