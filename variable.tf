c87d1acc218a52000b1a2c576e7ea248394736e1
Sourav Kundu <skundu.dev@gmail.com>
2023-11-20 23:02:29 -0600

    updated variables

--- variable.tf
+++ variable.tf
@@ -1,19 +1,28 @@
-variable "access_key" {
-  type      = string
-  default   = ""
-  sensitive = true
-}
-variable "secret_key" {
-  type      = string
-  default   = ""
-  sensitive = true
-}
 variable "region" {
-  type    = string
-  default = "us-east-2"
+  description = "AWS Cloud infrastructure region."
+  type        = string
+  default     = "us-east-2"
 }
-variable "private_ip_address" {
-  type    = string
-  default = "10.20.20.120"
-
+#Define IAM User Access Key
+variable "access_key" {
+  description = "The access_key that belongs to the IAM user."
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+#Define IAM User Secret Key
+variable "secret_key" {
+  description = "The secret_key that belongs to the IAM user."
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+variable "ami_name" {
+  description = "The ami name of the image from where the instances will be created"
+  default     = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
+  type        = list(string)
+}
+variable "instance_type" {
+  description = "The instance type of the EC2 instances"
+  default     = "t3.medium"
 }
