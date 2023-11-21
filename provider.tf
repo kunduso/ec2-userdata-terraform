ea6f8a755c23d8fbe5b515ede662796e0b7ca935
Sourav Kundu <skundu.dev@gmail.com>
2023-11-20 23:01:38 -0600

    added default tag

--- provider.tf
+++ provider.tf
@@ -10,4 +10,9 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+    default_tags {
+    tags = {
+      Source = "https://github.com/kunduso/ec2-userdata-terraform"
+    }
+  }
 }
