8fba8e393f5b71958455a27088590729592c1ba9
Sourav Kundu <skundu.dev@gmail.com>
2023-12-10 12:20:46 -0600

    updated code with use case specifics

--- ec2.tf
+++ ec2.tf
@@ -2,20 +2,13 @@ resource "aws_security_group" "instance-sg" {
   name        = "allow_access"
   description = "allow inbound traffic"
   vpc_id      = aws_vpc.this.id
-
-  # ingress {
-  #   description = "from my ip range"
-  #   from_port   = "3389"
-  #   to_port     = "3389"
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["147.219.191.0/24"]
-  # }
-  # egress {
-  #   cidr_blocks = ["0.0.0.0/0"]
-  #   from_port   = "0"
-  #   protocol    = "-1"
-  #   to_port     = "0"
-  # }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Enable access to the internet."
+  }
   tags = {
     "Name" = "app-1-ec2-sg"
   }
@@ -23,12 +16,13 @@ resource "aws_security_group" "instance-sg" {
 data "aws_ami" "windows-ami" {
   filter {
     name   = "name"
-    values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
+    values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
+    #values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
   }
-  filter {
-    name   = "platform"
-    values = ["windows"]
-  }
+  # filter {
+  #   name   = "platform"
+  #   values = ["windows"]
+  # }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -40,15 +34,10 @@ data "aws_ami" "windows-ami" {
 resource "aws_instance" "app-server" {
   instance_type               = "t2.micro"
   ami                         = data.aws_ami.windows-ami.id
-  vpc_security_group_ids      = [aws_security_group.web-pub-sg.id]
+  vpc_security_group_ids      = [aws_security_group.instance-sg.id]
   subnet_id                   = aws_subnet.public.id
-  key_name                    = "skundu-sandbox"
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  user_data = templatefile("user_data/user_data.tpl",
-    {
-      ServerName     = var.ServerName
-  })
   tags = {
     Name = "app-1-server-1"
   }
--- ssm_parameter.tf
+++
@@ -1,5 +0,0 @@
-# resource "aws_ssm_parameter" "parameter_one" {
-#   name  = "/dev/SecureVariableOne"
-#   type  = "SecureString"
-#   value = var.SecureVariableOne
-# }
