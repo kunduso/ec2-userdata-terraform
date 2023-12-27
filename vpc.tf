9664ebb3bedfa17f3bf96e03d543d090e7d50067
Sourav Kundu <skundu.dev@gmail.com>
2023-12-27 10:22:39 -0600

    renamed resources

--- vpc.tf
+++ vpc.tf
@@ -6,12 +6,12 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = true
   enable_dns_support = true
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.20.64/26"
   availability_zone = "us-east-2a"
   tags = {
-    "Name" = "app-1-public"
+    "Name" = "app-1-private"
   }
 }
 resource "aws_route_table" "this-rt" {
@@ -20,7 +20,7 @@ resource "aws_route_table" "this-rt" {
     "Name" = "app-1-route-table"
   }
 }
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.this-rt.id
 }
