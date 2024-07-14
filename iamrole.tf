188a7d4c8e2f8b5ef387260656e7502238d1e60b
Sourav Kundu <skundu.dev@gmail.com>
2024-07-13 22:16:40 -0500

    added permission to read ssm parameter

--- iamrole.tf
+++ iamrole.tf
@@ -33,3 +33,31 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "custom_policy_ssm" {
+  name        = "${var.name}-custom-policy-ssm"
+  path        = "/"
+  description = "A policy to allow Windows Amazon EC2 instances to talk with ssm parameters."
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:*",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_ssm_parameter.cloudwatch_windows_config.name}"
+      }
+    ]
+  })
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "policy_attachment_custom_ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.custom_policy_ssm.arn
+}
