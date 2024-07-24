ce0bca333e5f56b0e838eec9dfa01df314396197
Sourav Kundu <skundu.dev@gmail.com>
2024-07-23 21:39:54 -0500

    windows ami and add user data script

--- ec2.tf
+++ ec2.tf
@@ -16,13 +16,13 @@ resource "aws_security_group" "instance-sg" {
 data "aws_ami" "windows-ami" {
   filter {
     name   = "name"
-    values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
-    #values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
+    #values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
+    values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
   }
-  # filter {
-  #   name   = "platform"
-  #   values = ["windows"]
-  # }
+  filter {
+    name   = "platform"
+    values = ["windows"]
+  }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -33,13 +33,16 @@ data "aws_ami" "windows-ami" {
 
 data "aws_caller_identity" "current" {}
 resource "aws_instance" "app-server" {
-  instance_type               = "t2.micro"
+  instance_type               = "t3.medium"
   ami                         = data.aws_ami.windows-ami.id
   vpc_security_group_ids      = [aws_security_group.instance-sg.id]
   subnet_id                   = aws_subnet.private.id
   associate_public_ip_address = false
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  user_data = templatefile("user_data/user_data.tpl",)
+  user_data = templatefile("user_data/user_data.tpl",
+    {
+      Password = "${aws_ssm_parameter.ec2_password.value}"
+  })
   tags = {
     Name = "${var.name}-server-1"
   }
