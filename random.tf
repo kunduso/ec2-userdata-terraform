90a01d004f5f27254c711e3d4244ecbef0e2ff0b
Sourav Kundu <skundu.dev@gmail.com>
2024-07-26 20:39:31 -0500

    moved resource

--- random.tf
+++
@@ -1,6 +0,0 @@
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password
-resource "random_password" "auth" {
-  length           = 32
-  special          = true
-  override_special = "!&#$"
-}
--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,3 +1,9 @@
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password
+resource "random_password" "auth" {
+  length           = 32
+  special          = true
+  override_special = "!&#$"
+}
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
 resource "aws_ssm_parameter" "ec2_password" {
   name   = "/${var.name}/ec2-password"
