8efd4b58cc1b2f9f38eb2d70e689044893d3de9c
Sourav Kundu <skundu.dev@gmail.com>
2024-10-06 21:45:36 -0500

    #33 updated ssm parameter store with linux reference

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,8 +1,8 @@
 locals {
-  config_json = jsondecode(file("${path.module}/cloudwatch_config/config.json"))
+  config_json = jsondecode(file("${path.module}/cloudwatch_config/linux_config.json"))
 }
-resource "aws_ssm_parameter" "cloudwatch_windows_config" {
-  name  = "/${var.name}/Amazon-CloudWatch-Windows-Config"
+resource "aws_ssm_parameter" "cloudwatch_linux_config" {
+  name  = "/${var.name}/Amazon-CloudWatch-Linux-Config"
   type  = "String"
   value = jsonencode(local.config_json)
 }
