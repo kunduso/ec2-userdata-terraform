df88a58f7c7ce4611b9bbdf1a6a654e9b405a43f
Sourav Kundu <skundu.dev@gmail.com>
2024-10-06 21:45:53 -0500

    updated with linux reference

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,8 @@
-data "aws_ami" "windows-ami" {
+data "aws_ami" "linux_ami" {
   filter {
     name = "name"
-    #values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
-    values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
-  }
-  filter {
-    name   = "platform"
-    values = ["windows"]
+    values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
+    #values = ["Windows_Server-2019-English-Full-Base-2023*"] // for linux "amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"
   }
   filter {
     name   = "virtualization-type"
@@ -18,14 +14,14 @@ data "aws_ami" "windows-ami" {
 
 resource "aws_instance" "app-server" {
   instance_type               = "t2.micro"
-  ami                         = data.aws_ami.windows-ami.id
+  ami                         = data.aws_ami.linux_ami.id
   vpc_security_group_ids      = [aws_security_group.instance-sg.id]
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   user_data = templatefile("user_data/user_data.tpl",
     {
-      Parameter_Name = aws_ssm_parameter.cloudwatch_windows_config.name
+      Parameter_Name = aws_ssm_parameter.cloudwatch_linux_config.name
   })
   tags = {
     Name = "${var.name}-server-1"
