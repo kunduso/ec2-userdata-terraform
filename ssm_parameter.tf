e8b789eac5e35999478037930ae683d3d00ea192
Sourav Kundu <skundu.dev@gmail.com>
2024-10-07 16:55:29 -0500

    updated reference to use variables in config json #33

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,8 +1,10 @@
 locals {
-  config_json = jsondecode(file("${path.module}/cloudwatch_config/linux_config.json"))
+  config_json = templatefile("${path.module}/cloudwatch_config/linux_config.json.tftpl", {
+    log_group_name = aws_cloudwatch_log_group.logs.name
+  })
 }
 resource "aws_ssm_parameter" "cloudwatch_linux_config" {
   name  = "/${var.name}/Amazon-CloudWatch-Linux-Config"
   type  = "String"
-  value = jsonencode(local.config_json)
+  value = local.config_json
 }
