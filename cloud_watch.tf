157e444cde93a07732a24a98009f0d219d7d8daa
Sourav Kundu <skundu.dev@gmail.com>
2024-10-18 16:57:40 -0500

    correct cloudwatch log group #33

--- cloud_watch.tf
+++ cloud_watch.tf
@@ -1,5 +1,5 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "logs" {
-  name              = "/${var.name}/log"
+  name              = "/${var.name}/ec2/linux"
   retention_in_days = 365
 }
